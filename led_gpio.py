742b4ae74bcb007ff35dcd91e8628bbf66922f1f
bossware14 <bossware14@gmail.com>
2024-12-23 13:18:53 +0700

    Update led_gpio.py

--- led_gpio.py
+++ led_gpio.py
@@ -1,6 +1,7 @@
-from gpiozero import LED
+from gpiozero import MotionSensor , AngularServo , LED
 from signal import pause
 import gpiod
+import os
 from time import sleep
 
 # ฟังชั่น LED
@@ -8,21 +9,16 @@ def LED_API(LINE,TYPE,SEC=1):
   try:
     led = LED(LINE)
     if SEC == 0 and TYPE == 'on':
-      print("SEC 0")
+      print("SEC เปิดค้าง")
       led.on()
       pause()
     if TYPE == 'on' and SEC > 0:
        print("ON OFF")
        led.on()
        sleep(SEC)
-
-    if TYPE == 'loop' and SEC > 0:
+    if TYPE == 'loop':
        print("ON LOOP")
        led.blink(SEC,SEC,None,True)
-      
-    if TYPE = 'loop' and SEC == 0 :
-       print("ON LOOP SLEEP")
-       led.blink(0.1,0.1,None,True)
   finally:
     if SEC == 0 :
        return True
@@ -30,8 +26,8 @@ def LED_API(LINE,TYPE,SEC=1):
        return True
     led.off()
     sleep(1)
-    print("finally")
+    print("จบฟังชั้น")
 
-LED_API(22,'on',1) #เปิด 1 วิ
-LED_API(17,'loop',1) #กระพิบ
-LED_API(22,'on',0) #เปิดค้าง
+LED_API(22,'on',1)
+LED_API(17,'loop',1)
+LED_API(22,'on',0)
